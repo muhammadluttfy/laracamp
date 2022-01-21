4ecb0cdc6be8a12207304d75765b6ee574531942
muhammadluttfy <muhammadlutfi.storage@gmail.com>
2022-01-22 03:53:16 +0800

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::get('/success-checkout', function () {
   return view('success_checkout');
 })->name('success-checkout');
 
+
+
 Route::get('/dashboard', function () {
   return view('dashboard');
-})->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
